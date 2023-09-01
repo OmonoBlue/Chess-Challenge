bbd5120d3a3e7fc35be405155c356c7cd1a0c41d
OmonoBlue <extremeninjamaster@gmail.com>
2023-09-01 15:46:09 -0400

    Improved data loading

--- Chess-Challenge/src/My Bot/BotTrainer.cs
+++ Chess-Challenge/src/My Bot/BotTrainer.cs
@@ -11,13 +11,41 @@ using System.IO;
 using CsvHelper.Configuration;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Runtime.InteropServices;
+using CsvHelper.TypeConversion;
+using System.Text.Json.Nodes;
+using System.Text.Json;
+using CsvHelper.Configuration.Attributes;
+using System.Transactions;
 
 namespace Chess_Challenge.src.My_Bot
 {
     public class FenEvaluation
     {
         public string FEN { get; set; }
-        public string Evaluation { get; set; }
+        [TypeConverter(typeof(EvaluationConverter))]
+        public float Evaluation { get; set; }
+
+        public static float evalStr_to_float(string evalStr, float alpha = 300)
+        {
+            if (evalStr.StartsWith("#-"))
+            {
+                return -1f;
+            }
+            else if (evalStr.StartsWith("#+"))
+            {
+                return 1f;
+            }
+            int eval = Int32.Parse(evalStr, System.Globalization.NumberStyles.Integer);
+            return (float)Math.Tanh(eval / alpha);
+        }
+    }
+
+    public class EvaluationConverter : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            return FenEvaluation.evalStr_to_float(text);
+        }
     }
 
     public class BotTrainer
@@ -30,21 +58,20 @@ namespace Chess_Challenge.src.My_Bot
         const string testDataPath = "D:\\Documents\\_Programming\\C# Projects\\Chess-Challenge\\Chess-Challenge\\src\\My Bot\\training\\tactic_evals.csv";
         public const string modelPath = "D:\\Documents\\_Programming\\C# Projects\\Chess-Challenge\\Chess-Challenge\\src\\My Bot\\models\\savedmodel.nn";
 
-        private static string[,] fenEvalArray;
+        private static (string, float)[] fenEvalArray;
         private static Random random = new Random(1);
-        private const int numToLoad = 10000;
+        private const int numToLoad = -1;
         public static void Main(string[] args)
         {
             NeuralNetwork neuralNet = new(MyBot.NumInputs, MyBot.NumHiddenNeurons, 1, 1);
-            TrainMyNetwork(neuralNet, trainingPath, numToLoad, batchSize: 64, epochs: 32, learnRate: 0.05f, momentum: 0.9f, numThreads: 8);
-            //Console.WriteLine("Saving model...");
-            //neuralNet.Save(modelPath, true);
+            TrainMyNetwork(neuralNet, trainingPath, numToLoad, batchSize: 64, epochs: 64, learnRate: 0.05f, momentum: 0.9f, numThreads: 8);
+            Console.WriteLine("Saving model...");
+            neuralNet.Save(modelPath, true);
 
-            LoadCSVToArray(trainingPath, numToLoad);
             var randomPair = GetRandomFENEvalPair();
             ChessChallenge.API.Board board = ChessChallenge.API.Board.CreateBoardFromFEN(randomPair.Item1);
             float[] testinput = MyBot.getInputs(board);
-            float testtarget = evalStr_to_float(randomPair.Item2);
+            float testtarget = randomPair.Item2;
             float prediction = 0;
             try
             {
@@ -99,7 +126,8 @@ namespace Chess_Challenge.src.My_Bot
         {
             Console.WriteLine("Loading dataset...");
             LoadCSVToArray(datasetPath, numDatapoints);
-            (float[], float[])[] data = Enumerable.Range(0, numDatapoints).Select(pair => GetRandomFENEvalPair()).Select(pair => (MyBot.getInputs(ChessChallenge.API.Board.CreateBoardFromFEN(pair.Item1)), new float[] { evalStr_to_float(pair.Item2) })).ToArray();
+            Console.WriteLine("Parsing dataset...");
+            (float[], float[])[] data = fenEvalArray.Select(pair => (MyBot.getInputs(ChessChallenge.API.Board.CreateBoardFromFEN(pair.Item1)), new float[] { pair.Item2 })).ToArray();
             Console.WriteLine("Starting training...");
             neuralNet.Train(data, batchSize, epochs, learnRate, momentum, numThreads);
         }
@@ -122,11 +150,11 @@ namespace Chess_Challenge.src.My_Bot
                 {
                     float[] input;
                     float[] target;
-                    (string, string) currPair = GetRandomFENEvalPair();
+                    (string, float) currPair = GetRandomFENEvalPair();
                     try
                     {
                         input = MyBot.getInputs(ChessChallenge.API.Board.CreateBoardFromFEN(currPair.Item1));
-                        target = new float[]{evalStr_to_float(currPair.Item2) };
+                        target = new float[]{currPair.Item2};
                     } catch (Exception e)
                     {
                         Console.WriteLine($"Error {e}\ninput: {currPair}");
@@ -157,7 +185,7 @@ namespace Chess_Challenge.src.My_Bot
                 var randomPair = GetRandomFENEvalPair();
                 ChessChallenge.API.Board board = ChessChallenge.API.Board.CreateBoardFromFEN(randomPair.Item1);
                 float[] testinput = MyBot.getInputs(board);
-                float testtarget = evalStr_to_float(randomPair.Item2);
+                float testtarget = randomPair.Item2;
                 float prediction = testNet.Predict(testinput)[0];
                 float error = testNet.GetTotalError(new float[] { testtarget });
                 totalError += error;
@@ -179,41 +207,40 @@ namespace Chess_Challenge.src.My_Bot
             using (var reader = new StreamReader(path))
             using (var csv = new CsvReader(reader, new CsvConfiguration(System.Globalization.CultureInfo.InvariantCulture)))
             {
-                if (amount > 0) {
-                    records = csv.GetRecords<FenEvaluation>().Take(amount).ToList();
-                }
-                else
-                {
-                    records = csv.GetRecords<FenEvaluation>().ToList();
-                }
+                csv.Read();
+                csv.ReadHeader();
 
+                int rowCount = 0;
+                int skipped = 0;
+                while (csv.Read()) // Read the CSV file line by line
+                {
+                    if (amount > 0 && rowCount - skipped >= amount) break;
+                    
+                    try
+                    {
+                        records.Add(csv.GetRecord<FenEvaluation>());
+                        rowCount++;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error in row number {rowCount} of csv file. Total skipped: {++skipped}");
+                    }
+                }
             }
 
-            fenEvalArray = new string[records.Count, 2];
+            fenEvalArray = new (string, float)[records.Count];
             for (int i = 0; i < records.Count; i++)
             {
-                fenEvalArray[i, 0] = records[i].FEN;
-                fenEvalArray[i, 1] = records[i].Evaluation;
+                fenEvalArray[i] = (records[i].FEN, records[i].Evaluation);
             }
         }
 
-        public static (string, string) GetRandomFENEvalPair()
+        public static (string, float) GetRandomFENEvalPair()
         {
             int randomIndex = random.Next(0, fenEvalArray.GetLength(0));
-            return (fenEvalArray[randomIndex, 0], fenEvalArray[randomIndex, 1]);
+            return fenEvalArray[randomIndex];
         }
 
-        public static float evalStr_to_float(string evalStr, float alpha = 300)
-        {
-            if (evalStr.StartsWith("#-"))
-            {
-                return -1f;
-            } else if (evalStr.StartsWith("#+"))
-            {
-                return 1f;
-            }
-            int eval = Int32.Parse(evalStr, System.Globalization.NumberStyles.Integer);
-            return (float)Math.Tanh(eval / alpha);
-        }
+        
     }
 }
